ed023d020a0adf4e80ad00ebb068884e0cc6f810
Watataku <39507718+watataku8911@users.noreply.github.com>
2021-08-20 18:02:46 +0900

    fix/firebase-setting

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,14 +1,13 @@
-import firebase from "./firebase.config"
-
+import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-import "firebase/functions";
 import "firebase/storage";
+import firebaseConfig from "./firebase.config";
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
 export const db = firebase.firestore();
-export const functions = firebase.functions();
 export const storage = firebase.storage();
-export const fb = firebase;
-export const FirebaseFieldValue = firebase.firestore.FieldValue
 export const FirebaseTimestamp = firebase.firestore.Timestamp;
