3bc457ac11268b16f8d0d95351f5e5269a836c0e
Watataku <39507718+watataku8911@users.noreply.github.com>
2021-08-21 11:58:38 +0900

    fix/auth

--- src/views/Deleted.vue
+++ src/views/Deleted.vue
@@ -3,10 +3,18 @@
 </template>
 
 <script>
+import { auth } from "../firebase/index";
 import DeletedComponent from "../components/DeletedComponent.vue";
 export default {
   components: {
     DeletedComponent,
+  },
+  mounted() {
+    auth.onAuthStateChanged((user) => {
+      if (user == null) {
+        this.$router.push("/signin");
+      }
+    });
   },
 };
 </script>
